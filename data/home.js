175de0845e230d28fe1d7f683a0ed86db0913832
shahrour <ahmadshahrour951@gmail.com>
2020-10-18 00:34:02 -0700

    Create data structure for home page

---
+++ data/home.js
@@ -0,0 +1,108 @@
+const data = {
+  residences: [
+    {
+      label: 'Herbert Hotel Residence',
+      floors: [
+        {
+          label: 'Floor 2',
+          students: [
+            {
+              img: 'rafa.jpeg',
+              name: 'Rafa Vazquez',
+              dorm_room: '214',
+              top_things: ['Games', 'Movies', 'Hiking'],
+            },
+            {
+              img: 'samuel.png',
+              name: 'Samuel Madrigal',
+              dorm_room: '214',
+              top_things: ['Games', 'Streaming', 'Food'],
+            },
+          ],
+        },
+        {
+          label: 'Floor 4',
+          students: [
+            {
+              img: 'joshua.png',
+              name: 'Joshua Riley',
+              dorm_room: '411',
+              top_things: ['Outings', 'Clothes', 'Architecture'],
+            },
+          ],
+        },
+        {
+          label: 'Floor 5',
+          students: [
+            {
+              img: 'nolan.png',
+              name: 'Sergio Rodriguez',
+              dorm_room: '507',
+              top_things: ['Guitar', 'Movies', 'Physics'],
+            },
+            {
+              img: 'nolan.png',
+              name: 'Nolan Ngo',
+              dorm_room: '507',
+              top_things: ['Guitar', 'Movies', 'Physics'],
+            },
+            {
+              img: 'nolan.png',
+              name: 'Dino Martinez',
+              dorm_room: '511',
+              top_things: ['Guitar', 'Movies', 'Physics'],
+            },
+            {
+              img: 'ignacio.jpeg',
+              name: 'Ignacio Jimenez',
+              dorm_room: '511',
+              top_things: ['Guitar', 'Movies', 'Physics'],
+            },
+            {
+              img: 'nolan.png',
+              name: 'Edison Li',
+              dorm_room: '517',
+              top_things: ['Guitar', 'Movies', 'Physics'],
+            },
+            {
+              img: 'rick.jpeg',
+              name: 'Rick Jacobson',
+              dorm_room: '517',
+              top_things: ['Guitar', 'Movies', 'Physics'],
+            },
+          ],
+        },
+        {
+          label: 'Floor 6',
+          students: [
+            {
+              img: 'ahmed.jpeg',
+              name: 'Ahmed Shahrour',
+              dorm_room: '611',
+              top_things: ['Philosophy', 'Music', 'Physics'],
+            },
+            {
+              img: 'kupono.jpeg',
+              name: 'Kupono Kealiinohomoku',
+              dorm_room: '617',
+              top_things: ['Games', 'Movies', 'Hiking'],
+            },
+          ],
+        },
+        {
+          label: 'Floor 7',
+          students: [
+            {
+              img: 'ruot.jpeg',
+              name: 'Ruot Nyak',
+              dorm_room: '717',
+              top_things: ['Games', 'Movies', 'Hiking'],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export default data;
